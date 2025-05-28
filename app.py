90b269c054a9eac2a06df97d38e9b129a0d6d843
uniqueateme <temethebest5@gmail.com>
2025-05-27 21:26:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
-import hashlib
-import base58
+import re
 app = Flask(_name_)
 BASE_URL = "https://hashkeys.space/71/"
-VERSION_MAINNET = b'\x00'
-def generate_p2pkh_address(public_key_hash):
-    payload = VERSION_MAINNET + public_key_hash
-    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-    address = base58.b58encode(payload + checksum).decode()
-    return address
 def scrape_addresses():
     response = requests.get(BASE_URL)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -20,8 +13,8 @@ def scrape_addresses():
         columns = row.find_all('td')
         if len(columns) >= 2:
             key_hex = columns[0].text.strip()
-            balance = columns[1].text.strip()
-            addresses.append((key_hex, balance))
+            address = columns[1].text.strip()
+            addresses.append((key_hex, address))
     return addresses
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -29,14 +22,10 @@ def home():
     if request.method == 'POST':
         address_to_check = request.form.get('address')
         addresses = scrape_addresses()
-        for key_hex, balance in addresses:
-            generated_address = generate_p2pkh_address(bytes.fromhex(key_hex))
-            if generated_address == address_to_check:
-                found_address = (key_hex, generated_address, balance)
+        for key_hex, address in addresses:
+            if address == address_to_check:
+                found_address = (key_hex, address)
                 break
-        filtered_addresses = [(kh, bal) for kh, bal in addresses if float(bal) > 0]
-        return render_template('index.html', found_address=found_address, filtered_addresses=filtered_addresses)
-    
-    return render_template('index.html', found_address=None, filtered_addresses=[])
+    return render_template('index.html', found_address=found_address)
 if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001)
